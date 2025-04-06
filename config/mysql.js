6b83cbf3695c3cd5533738887d25b5443b3b0974
Diego Andres Galvis Villamizar <diego.galvis@wiedii.co>
2025-04-06 12:25:23 -0500

    comment mysql connection

--- config/mysql.js
+++ config/mysql.js
@@ -1,20 +1,20 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'cursoBot'
-})
+// const connection = mysql.createConnection({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: '',
+//     database: 'cursoBot'
+// })
 
-connection.connect(err => {
-    if(err){
-        console.log('Error de conexión con la base de datos', err);
-        return
-    }
+// connection.connect(err => {
+//     if(err){
+//         console.log('Error de conexión con la base de datos', err);
+//         return
+//     }
 
-    console.log('Conectado con la base de datos.')
-})
+//     console.log('Conectado con la base de datos.')
+// })
 
 
-module.exports = connection
+// module.exports = connection
